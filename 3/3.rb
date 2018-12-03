9b1ef015de0c0f91b0671192c7c4eedb1a19b30d
Dário Tavares Antunes <dario.antunes@intercom.io>
2018-12-03 13:30:11 +0000

    Solution for D3P2

--- 3/3.rb
+++ 3/3.rb
@@ -1,9 +1,11 @@
 class Claim
 	PARSE_PATTERN = /^#(\d+) @ (\d+),(\d+): (\d+)x(\d+)$/
 	attr_reader :id, :x, :y, :w, :h
-	
+	attr_accessor :overlapped
+
 	def initialize(str)
 		values = Claim::PARSE_PATTERN.match(str)
+		@overlapped = false
 		@id = values[1].to_i
 		@x = values[2].to_i
 		@y = values[3].to_i
@@ -14,11 +16,17 @@ end
 
 def fill_cloth(claims)
 	cloth = Hash.new(0)
-	claims.each { |claim|
+	overlap_bitmap = {}
+	claims.each { |id, claim|
 		(0..claim.w-1).each { |w|
 			(0..claim.h-1).each { | h|
 				key = [claim.x + w, claim.y + h]
 				cloth[key] = cloth[key] + 1
+				if overlap_bitmap.key? key
+					claims[overlap_bitmap[key]].overlapped = true
+					claim.overlapped = true
+				end
+				overlap_bitmap[key] = id
 			}
 		}
 	}
@@ -26,13 +34,19 @@ def fill_cloth(claims)
 end
 
 def parse_claims(file)
-	claims = file.read.chomp.split("\n").map{|str| Claim.new(str)}
+	Hash[file.read.chomp.split("\n").map{|str| Claim.new(str)}.map{|claim| [claim.id, claim]}]
 end
 
-def solution_1(filename)
-	claims = parse_claims(File.new(filename))
-	cloth = fill_cloth(claims)
+def solution_1(cloth)
 	cloth.select{ |_,v| v >= 2 }.length
 end
 
-puts solution_1("input")
+def solution_2(claims)
+	claims.values.reject(&:overlapped).first
+end
+
+claims = parse_claims(File.new("input"))
+cloth = fill_cloth(claims)
+
+puts solution_1(cloth)
+puts solution_2(claims).id
