4b4ac350d26696b852886fb3956454a24a3c5fbf
Dário Tavares Antunes <dario@ntun.es>
2021-12-11 16:42:56 +0000

    Add grid representation with update DSL for reuse

--- 2021/9/9.rb
+++ 2021/9/9.rb
@@ -1,28 +1,11 @@
-def adjacent_positions(row_ind, col_ind, board)
-  row_max = board.length
-  col_max = board.first.length
-  positions = []
+require '../shared/grid.rb'
 
-  positions << [row_ind - 1, col_ind] unless row_ind.zero?
-  positions << [row_ind + 1, col_ind] unless row_ind + 1 == row_max
-  positions << [row_ind, col_ind - 1] unless col_ind.zero?
-  positions << [row_ind, col_ind + 1] unless col_ind + 1 == col_max
-
-  positions
-end
-
-def problem_1(board)
+def problem_1(grid)
   risk = 0
 
-  board.each_with_index do |row, row_ind|
-    row.each_with_index do |val, col_ind|
-      is_low_point = adjacent_positions(row_ind, col_ind, board).all? do |row_ind, col_ind|
-        board[row_ind][col_ind] > val
-      end
-
-      if is_low_point
-        risk += val + 1
-      end
+  grid.each_cell do
+    if each_adjacent(include_diagonal: false).all? { |other| other.val > val }
+      risk += val + 1
     end
   end
 
@@ -32,35 +15,34 @@ end
 # This is simpler than the problem as stated. All non-9 values are necessarily in a basin, so
 # there's no need to check whether a number is adjacent to numbers in a basin, we just need to
 # find all numbers that are enclosed by board boundaries or 9s.
-def problem_2(board)
-  def find_basin(board, row_ind, col_ind, visited, indent=" ")
-    return 0 if visited[row_ind][col_ind]
+def problem_2(grid)
+  def find_basin(grid, cell, visited)
+    visited.at(row_ind: cell.row, col_ind: cell.col) do
+      return 0 if val
 
-    visited[row_ind][col_ind] = true
-    return 0 if board[row_ind][col_ind] == 9
+      self.val = true
+    end
+    return 0 if cell.val == 9
 
-    positions = adjacent_positions(row_ind, col_ind, board)
-    1 + positions.map { |oth_row_ind, oth_col_ind| find_basin(board, oth_row_ind, oth_col_ind, visited, indent + " ") }.sum
+    1 + cell.each_adjacent(include_diagonal: false).map { |other| find_basin(grid, other, visited) }.sum
   end
 
   # The more natural [[false] * board.first.length] * board.length creates multiple copies of the
   # same array, such that visiting the first column of the first row visits the first column of all
   # rows.
-  visited = (0...board.length).map { [false] * board.first.length }
+  visited = grid.indicator_board
 
   basin_sizes = []
-  board.each_with_index do |row, row_ind|
-    row.each_with_index do |val, col_ind|
-      next if visited[row_ind][col_ind]
+  grid.each_cell do
+    next if visited.at(row_ind: row, col_ind: col).val
 
-      basin_sizes << find_basin(board, row_ind, col_ind, visited)
-    end
+    basin_sizes << find_basin(grid, self, visited)
   end
 
   basin_sizes.sort.last(3).reduce(:*)
 end
 
-board = File.readlines("input", chomp: true).map { |line| line.split("").map(&:to_i) }
+board = Grid.new(File.readlines("input", chomp: true).map { |line| line.split("").map(&:to_i) })
 
 puts "Problem 1: #{problem_1(board)}"
 puts "Problem 2: #{problem_2(board)}"
---
+++ 2021/shared/grid.rb
@@ -0,0 +1,98 @@
+class Grid
+  class Cell
+    attr_accessor :val
+    attr_reader :row, :col
+
+    def initialize(val, row, col)
+      @val = val
+      @row = row
+      @col = col
+    end
+    
+    def set_adjacent(sides, diagonals)
+      @sides = sides
+      @diagonals = diagonals
+    end
+
+    def each_adjacent(include_diagonal:, &block)
+      return enum_for(__callee__, include_diagonal: include_diagonal) unless block_given?
+
+      @sides.each { |cell| cell.instance_eval(&block) }
+
+      if include_diagonal
+        @diagonals.each { |cell| cell.instance_eval(&block) }
+      end
+
+      nil
+    end
+  end
+
+  def initialize(board)
+    @board = board
+
+    # Convert raw values to cells for the DSL
+    @board.each_with_index do |row, row_ind|
+      row.each_with_index do |val, col_ind|
+        @board[row_ind][col_ind] = Cell.new(val, row_ind, col_ind)
+      end
+    end
+
+    # Set adjacencies
+    @board.each_with_index do |row, row_ind|
+      row.each_with_index do |cell, col_ind|
+        row_max = @board.length
+        col_max = @board.first.length
+        sides = []
+
+        sides << @board[row_ind - 1][col_ind] unless row_ind.zero?
+        sides << @board[row_ind + 1][col_ind] unless row_ind + 1 == row_max
+        sides << @board[row_ind][col_ind - 1] unless col_ind.zero?
+        sides << @board[row_ind][col_ind + 1] unless col_ind + 1 == col_max
+
+        diagonals = []
+        diagonals << @board[row_ind - 1][col_ind - 1] if in_bounds?(row_ind - 1, col_ind - 1)
+        diagonals << @board[row_ind - 1][col_ind + 1] if in_bounds?(row_ind - 1, col_ind + 1)
+        diagonals << @board[row_ind + 1][col_ind - 1] if in_bounds?(row_ind + 1, col_ind - 1)
+        diagonals << @board[row_ind + 1][col_ind + 1] if in_bounds?(row_ind + 1, col_ind + 1)
+
+        cell.set_adjacent(sides, diagonals)
+      end
+    end
+  end
+
+  def indicator_board
+    # The more natural [[false] * board.first.length] * board.length creates multiple copies of the
+    # same array, such that the first cell of the first row references the same value as the first
+    # cell of every other row.
+    Grid.new((0...@board.length).map { [false] * @board.first.length })
+  end
+
+  def each_cell(&block)
+    return enum_for(__callee__) unless block_given?
+
+    @board.each_with_index do |row, row_ind|
+      row.each_with_index do |val, col_ind|
+        val.instance_eval(&block)
+      end
+    end
+  end
+
+  def at(row_ind:, col_ind:, &block)
+    raise "Out of bounds get" unless in_bounds?(row_ind, col_ind)
+
+    if block_given?
+      @board[row_ind][col_ind].instance_eval(&block)
+    else
+      @board[row_ind][col_ind]
+    end
+  end
+
+  def display(&val_display)
+    @board.map { |row| row.map { |val| val_display.call(cell.val) }.join }.join("\n")
+  end
+
+  private def in_bounds?(row_ind, col_ind)
+    row_ind >= 0 && row_ind < @board.length &&
+      col_ind >= 0 && col_ind < @board.first.length
+  end
+end
